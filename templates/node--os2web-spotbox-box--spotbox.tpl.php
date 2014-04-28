e0fd101418993261b320ae7a104a43d267a9668a
Anders Bryrup <andersbryrup@gmail.com>
2014-04-28 15:39:04 +0200

    Add panel pane to show spotboxes. This makes views obsolete.

--- templates/node--os2web-spotbox-box--spotbox.tpl.php
+++ templates/node--os2web-spotbox-box--spotbox.tpl.php
@@ -1,5 +1,6 @@
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="caption">
+  <?php $spotbox_url = (empty($spotbox_url) && isset($variables['elements']['#spotbox_url'])) ? $variables['elements']['#spotbox_url'] : $spotbox_url; ?>
     <?php if(!empty($spotbox_url)) : ?>
       <a href="<?php print $spotbox_url ?>">
     <?php endif; ?>
